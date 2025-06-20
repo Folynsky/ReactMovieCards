60e9e574165dab29cd4934ca93d2fc1e6a0ea940
Folynsky <kevin.foly06@gmail.com>
2025-06-19 21:10:19 -0600

    Ruteo y navbar

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites(){
+    return <div className="favorites-empty">
+        <h2>No Favorite Movies yet</h2>
+        <p>Start adding movies to your and they will appear here</p>
+    </div>
+}
+
+export default Favorites;
